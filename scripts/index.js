f224e0567b560d1041b3be4d88a599408435e3d4
John Botero <jhondark@gmail.com>
2020-05-14 18:18:37 -0500

    fixed image icon-error inputs

--- scripts/index.js
+++ scripts/index.js
@@ -5,13 +5,13 @@ function validation() {
     var value = document.getElementById("name").value;
     if( value == null || value.length == 0 || /^\s+$/.test(value) ) {
         document.getElementById("text_vali").style.display="inline"
-        document.getElementById("name").style.backgroundImage="url(../images/icon-error.svg)"
+        document.getElementById("name").style.backgroundImage="url(./images/icon-error.svg)"
 
     } else
     var value2 = document.getElementById("lname").value;
     if( value2 == null || value2.length == 0 || /^\s+$/.test(value2) ) {
         document.getElementById("last_vali").style.display="inline"
-        document.getElementById("lname").style.backgroundImage="url(../images/icon-error.svg)"
+        document.getElementById("lname").style.backgroundImage="url(./images/icon-error.svg)"
 
     } else
     var value3 = document.getElementById("mail").value
